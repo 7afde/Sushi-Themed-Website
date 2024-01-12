2fb0251511c068af9f0de22ee09857e95d416011
hafed <hafed.zouyed@univ-constantine2.dz>
2024-01-12 19:39:09 +0100

    I added animations until the popular food section

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
-// import AOS from "aos";
-// import "aos/dist/aos.css";
+// import images as relative image path won't work with vite/vercel
+import check from '../assets/check.svg'
+import star from '../assets/star.svg'
+import sushi12 from '../assets/sushi-12.png'
+import sushi11 from '../assets/sushi-11.png'
+import sushi10 from '../assets/sushi-10.png'
 
-// // init AOS animation
-// AOS.init({
-//   duration: 1000,
-//   offset: 100,
-// });
+import AOS from "aos"
+import "aos/dist/aos.css"
+// init AOS animation
+AOS.init({
+  duration: 1000,
+  offset: 100,
+});
